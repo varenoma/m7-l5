0400b11a6a05852183fa9dc846b165abdb1553d3
Shukurullo-py <mamadaliyevshukurullo85@gmail.com>
2025-05-23 18:39:53 +0500

    yangilandi

--- comment/views.py
+++ comment/views.py
@@ -9,4 +9,4 @@ class CommentViewSet(ModelViewSet):
     queryset = CommentModels.objects.all()
     serializer_class = CommentSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_class = CommentFilter
+    filterset_class = CommentFilter  # http://127.0.0.1:8000/comments/comments/?post=2
